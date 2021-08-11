8c7a69fbb57a784605f633437903f16f5b95e8dd
lmilburn <66505573+lmilburn@users.noreply.github.com>
2021-08-11 15:01:21 -0700

    inventory fix

--- hypebeast/tester.py
+++ hypebeast/tester.py
@@ -29,24 +29,47 @@ async def on_ready():
 @bot.command(name='daily', help='Once every 24 hours, roll for a free item')
 @commands.cooldown(1.0, 86400.0, commands.BucketType.user)
 async def daily_item(ctx):
-    with open('sample.json') as json_file:
-      itemDict = json.load(json_file)
-    itemTup = select_random_item(itemDict)
-    if itemTup[1] != "None":
-      response = "You received **{0}** with a value of **{1}**.".format(itemTup[0], itemTup[1])
-      await ctx.send(response)
-    else:
-      response = "You tried to acquire **{0}** but it was solid out. Tough luck!".format(itemTup[0])
-      await ctx.send(response)
+  with open('sample.json') as json_file:
+    itemDict = json.load(json_file)
+  itemTup = select_random_item(itemDict)
+  if itemTup[1] != "None":
+    response = "You received **{0}** with a value of **{1}**.".format(itemTup[0], itemTup[1])
+    await open_account(ctx.author)
+    users = await get_bank_data()
+    users[str(ctx.author.id)]["items"][itemTup[0]] = itemTup[1]
+    with open("mainbank.json", "w") as f:
+          json.dump(users, f)
+    await ctx.send(response)
+  else:
+    response = "You tried to acquire **{0}** but it was sold out. Tough luck!".format(itemTup[0])
+    await ctx.send(response)
 
 
 @daily_item.error
 async def bot_error(ctx, error):
-    if isinstance(error, commands.CommandOnCooldown):
-        timeInHours = (error.retry_after / 60)/60
-        timeInMinutes = (timeInHours-int(timeInHours))*60 
-        msg = 'You have already claimed today, please try again in {} hours and {} minutes'.format(int(timeInHours), int(timeInMinutes))
-        await ctx.send(msg)
+  if isinstance(error, commands.CommandOnCooldown):
+    timeInHours = (error.retry_after / 60)/60
+    timeInMinutes = (timeInHours-int(timeInHours))*60 
+    msg = 'You have already claimed today, please try again in {} hours and {} minutes'.format(int(timeInHours), int(timeInMinutes))
+    await ctx.send(msg)
+
+async def get_bank_data():
+  with open("mainbank.json", "r") as bank:
+    users = json.load(bank)
+  return users
+
+async def open_account(user):
+  users = await get_bank_data()
+  if str(user.id) in users:
+    return False
+  else:
+    users[str(user.id)] = {}
+    users[str(user.id)]["usd_balance"] = 0
+    users[str(user.id)]["items"] = {}
+    
+  with open("mainbank.json", "w") as bank:
+    json.dump(users, bank)
+  return True
 
 bot.run(TOKEN)
 
